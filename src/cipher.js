b88ca0527b771d48011b72fa35026b779722f89b
macarenasaez <makadaira@gmail.com>
2019-06-24 08:31:00 -0400

    actualizacion de proyecto

--- src/cipher.js
+++ src/cipher.js
@@ -1,9 +1,47 @@
 window.cipher = {
-  encode: () => {
-    /* Acá va tu código */
-    
+  encode: (texto,numero) => {
+   let mensajefinal = "";
+    for (let i = 0; i < texto.length; i++){
+      
+      let numerodeletra = texto.charCodeAt(i);
+
+      //console.log(numerodeletra);
+      if(numerodeletra >= 65 && numerodeletra <= 90){
+      let nuevonumero = numerodeletra- 65 + numero % 26 + 65;
+      mensajefinal += String.fromCharCode(nuevonumero);
+      console.log(mensajefinal);
+    }
+  } 
+    return mensajefinal;
+
   },
-  decode: () => {
-    /* Acá va tu código */
+
+
+  decode: (texto,numero) => {
+   let mensajefinal = "";
+
+   for (let i = 0; i < texto.length; i++) {
+    let numerodeletra = texto.charCodeAt(i);
+    //console.log(numerodeletro)
+
+   if (numerodeletra >= 65 && numerodeletra >= 90){
+
+    let nuevonumero = (numerodeletra - 90 - numero) % 26 + 90;
+    mensajefinal += String.fromCharCode(nuevonumero);
+   }
+
+} // cierre de for
+return mensajefinal;
+
+
+
+
+
+
+
+
   }
-};
+
+  };//cierre de decode
+  
+
